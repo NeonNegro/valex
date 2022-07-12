625880a07789383ffea22dbad10ff3807fc262c5
Rodrigo Chaves <rodrigoccavalcante@gmail.com>
2022-07-12 16:40:28 -0300

    routers&controllers

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,8 @@ const app = express();
 app.use(json());
 app.use(cors());
 app.use(router);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`);
+})
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { errorHandlerMiddleware } from "../middlewares/errorHandlerMiddleware.js";
 import cardRouter from "./cardRouter.js";
+import paymentRouter from "./paymentRouter.js";
+import rechargeRouter from "./rechargeRouter.js";
 
 
 const router = Router();
 
 router.use(cardRouter);
+router.use(rechargeRouter);
+router.use(paymentRouter);
 
 
 router.use(errorHandlerMiddleware);
